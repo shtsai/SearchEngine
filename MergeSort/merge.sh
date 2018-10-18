443d5fb9add8b6b9f1121ed16568cf6d43e255d1
Shang-Hung Tsai <shtsai7@gmail.com>
2018-10-18 13:28:47 -0400

    update script for merge intermediate files

--- MergeSort/merge.sh
+++ MergeSort/merge.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sort -n -t ',' -k1,1 -k2,2 -o postings.txt posting*
+# Merge postings
+sort -m -n -t ',' -k1,1 -k2,2 -o postings.txt posting*
+
+# Merge page table
+sort -m -n -t ',' -k1 -o page_table.txt page_table*
+
+# Merge term table
+sort -m -n -t ',' -k1 -o term_table.txt term_table*
